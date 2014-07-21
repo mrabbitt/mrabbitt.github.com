ac93b662a06560541849075ddf629efd7727994b
Michael Rabbitt <rabbim@alum.rpi.edu>
2014-07-21 15:11:13 -0400

    Revert "Migrated to Nikola v7.0.1."  Several issues.
    This reverts commit fd4c3145e6bbd3dd2a8e88c7678d18b50d7722ff.

--- assets/js/mathjax.js
+++ assets/js/mathjax.js
@@ -5,12 +5,8 @@
 window.onload = function () {
   setTimeout(function () {
     var script = document.createElement("script");
-    if (location.protocol == 'https:') {
-        scriptbase = "https://c328740.ssl.cf1.rackcdn.com/";
-    } else {
-        scriptbase = "http://cdn.mathjax.org/";
-    }
-    script.src  = scriptbase + "mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+    script.type = "text/javascript";
+    script.src  = "https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS_HTML";
     document.getElementsByTagName("body")[0].appendChild(script);
   },1)
 }
